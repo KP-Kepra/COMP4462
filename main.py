28fd8617160bc1a7eedf7f5c14f67dbe698ee2a8
kp-kepra <kevinpr153@gmail.com>
2019-05-02 21:25:47 +0800

    update

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 import json
 import os
 
-for i in range(1, 13):
-  raw_data = pd.read_csv("gta+dota+tf2/new_2018_{:02d}.csv".format(i))
+for i in range(8, 13):
+  raw_data = pd.read_csv("ow-lol-pubg-csgo/new_2018_{:02d}.csv".format(i))
 
   old_csv_path = '200_2018_{:02d}.csv'.format(i)
 
@@ -13,17 +13,16 @@ for i in range(1, 13):
 
   # raw_smash_data = raw_data[raw_data.subreddit == 'smashbros'].head(200)
   # raw_hearthstone = raw_data[raw_data.subreddit == 'hearthstone'].head(200)
-  # raw_ow_data = raw_data[raw_data.subreddit == 'Overwatch'].head(200)
-  # raw_lol_data = raw_data[raw_data.subreddit == 'leagueoflegends'].head(200)
-  # raw_pubg_data = raw_data[raw_data.subreddit == 'PUBATTLEGROUNDS'].head(200)
-  # raw_csgo_data = raw_data[raw_data.subreddit == 'GlobalOffensive'].head(200)
+  raw_ow_data = raw_data[raw_data.subreddit == 'Overwatch'].head(200)
+  raw_lol_data = raw_data[raw_data.subreddit == 'leagueoflegends'].head(200)
+  raw_pubg_data = raw_data[raw_data.subreddit == 'PUBATTLEGROUNDS'].head(200)
+  raw_csgo_data = raw_data[raw_data.subreddit == 'GlobalOffensive'].head(200)
+  # raw_dota_data = raw_data[raw_data.subreddit == 'DotA2'].head(200)
+  # raw_gta_data = raw_data[raw_data.subreddit == 'GrandTheftAutoV'].head(200)
+  # raw_tf_data = raw_data[raw_data.subreddit == 'tf2'].head(200)
 
-  raw_dota_data = raw_data[raw_data.subreddit == 'DotA2'].head(200)
-  raw_gta_data = raw_data[raw_data.subreddit == 'GrandTheftAutoV'].head(200)
-  raw_tf_data = raw_data[raw_data.subreddit == 'tf2'].head(200)
-
-  # raw_data = pd.concat([raw_ow_data, raw_lol_data, raw_pubg_data, raw_csgo_data])
-  raw_data = pd.concat([raw_dota_data, raw_gta_data, raw_tf_data])
+  raw_data = pd.concat([raw_ow_data, raw_lol_data, raw_pubg_data, raw_csgo_data])
+  # raw_data = pd.concat([raw_dota_data, raw_gta_data, raw_tf_data])
   # raw_data = pd.concat([raw_smash_data, raw_hearthstone])
   if old_200_csv is not None:
     raw_data = pd.concat([old_200_csv, raw_data])
