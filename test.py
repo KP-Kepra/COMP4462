264af3f98ff8ae94bca58181988e666fc6b6c007
kp-kepra <kevinpr153@gmail.com>
2019-05-06 14:36:54 +0800

    edited file

--- test.py
+++ test.py
@@ -6,12 +6,15 @@ import os
 for i in range(1, 13):
   print(i)
   df = pd.read_csv('200_2018_{:02d}.csv'.format(i))
+  df['month'] = i
 
   while 'index' in df.columns:
     del df['index'] 
 
   while 'Unnamed: 0' in df.columns:
     del df['Unnamed: 0']
+
+  df = df[['subreddit', 'length', 'score', 'month']]
 
   print(df[df.subreddit == 'Overwatch'].shape)
   print(df[df.subreddit == 'leagueoflegends'].shape)
@@ -23,3 +26,4 @@ for i in range(1, 13):
   print(df[df.subreddit == 'GrandTheftAutoV'].shape)
   print(df[df.subreddit == 'tf2'].shape)
 
+  df.to_csv('200_2018_{:02d}.csv'.format(i))
